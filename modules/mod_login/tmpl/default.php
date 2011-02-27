d33b873d8c754a3191ff48f4ebc1a8d0ddcc2c61
jar.patryk <jar.patryk@b36344ac-a876-f84d-e60f-d41b6b5e09fb>
2011-02-27 01:18:36 +0000

    zmiana polozenia wyszukiwarki, wczytywanie jquery na samym poczatku, ostylowanie i logka client-side wyszukiwarki

--- modules/mod_login/tmpl/default.php
+++ modules/mod_login/tmpl/default.php
@@ -44,7 +44,6 @@
 
 	<?php else : ?>
 
-	<script src="http://code.jquery.com/jquery-1.5.1.min.js" type="text/javascript"></script>
 	<script type="text/javascript">
 	$(document).ready(function() {
 		$('#login-form').hide();
