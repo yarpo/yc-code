321c907fdee070c975ebb6cc87ccb8f9c5cb74e2
jar.patryk <jar.patryk@b36344ac-a876-f84d-e60f-d41b6b5e09fb>
2011-02-28 23:33:04 +0000

    zmiany w listach, sformatowanie kodu w tabeli

--- trunk/modules/mod_blog_calendar/js/blog_calendar.js
+++ trunk/modules/mod_blog_calendar/js/blog_calendar.js
@@ -2,26 +2,35 @@ var modid;
 var key;
 
 window.addEvent('domready', function(){
-var links = document.getElementsByTagName('a');
+	var links = document.getElementsByTagName('a');
 
-for(key in links){
+	for(key in links){
 
-	if(typeof(links[key]) == "object"){
-		if(links[key].id.substring(0,10)=='prevMonth-'){
-			modid= links[key].id.substring(10);
-			links[key].onclick = function(){month--; newDate(month,year,modid);}
-			links[key].href='javascript:void(0)';
+		if(typeof(links[key]) == "object"){
+			if(links[key].id.substring(0,10)=='prevMonth-')
+			{
+				modid= links[key].id.substring(10);
+				links[key].onclick = function()
+				{
+					month--;
+					newDate(month,year,modid);
+				}
+				links[key].href='javascript:void(0)';
 			}
 
 
-		if(links[key].id.substring(0,10)=='nextMonth-'){
-			links[key].onclick = function(){month++; newDate(month,year,modid);}
-			links[key].href='javascript:void(0)';
+			if(links[key].id.substring(0,10)=='nextMonth-')
+			{
+				links[key].onclick = function()
+				{
+					month++;
+					newDate(month,year,modid);
+				}
+				links[key].href='javascript:void(0)';
 			}
-
 		}
 	}
-})
+});
 
 function newAjax()
 {
@@ -48,7 +57,6 @@ function newAjax()
 
 function newDate(month,year,modid)
 {
-		
 		var myFx = new Fx.Style('tableCalendar-'+modid, 'opacity').start(0);
 		loadHtml  = "<p id='loadingDiv-"+modid+"' style='margin-left: 1cm; margin-top: -2cm; margin-bottom: 2cm;'>";
 		loadHtml += "<img src='"+calendar_baseurl+"modules/mod_blog_calendar/img/loading.gif'>";
@@ -57,15 +65,15 @@ function newDate(month,year,modid)
 		var myFx = new Fx.Style('tableCalendar-'+modid, 'opacity').start(1,0.05);
 		
 		if(month<=0){
-		month+=12;
-		year--;
+			month+=12;
+			year--;
 		}
 		if(month>12){
-		month-=12;
-		year++;
+			month-=12;
+			year++;
 		}
-		
-		var ajax=newAjax();
+
+		var ajax = newAjax();
 		ajax.open("POST", location.href, true);
 		ajax.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
 		ajax.send('year='+year+'&month='+month+'&ajaxCalMod=1'+'&ajaxmodid='+modid);
@@ -77,19 +85,18 @@ function newDate(month,year,modid)
 				var response = ajax.responseText;
 				var start = response.indexOf('<!--calendar-'+modid+' start-->');
 				var finish = response.indexOf('<!--calendar-'+modid+' end-->');
-				
+
 				justTheCalendar= response.substring(start,finish);
-				
 				var myFx = new Fx.Style('tableCalendar-'+modid, 'opacity').start(0.3,1);
 				document.getElementById( 'calendar-'+modid ).innerHTML=justTheCalendar;
-				
+
 				linkPrev= document.getElementById('prevMonth-'+modid);
 				linkNext= document.getElementById('nextMonth-'+modid);
-				
+
 				linkPrev.onclick= function(){month--; newDate(month,year,modid);}
 				linkNext.onclick= function(){month++; newDate(month,year,modid);}
-				
-				linkNext.href= linkPrev.href= 'javascript:void(0)';
+
+				//linkNext.href= linkPrev.href= 'javascript:void(0)';
 			}
 		}
 	}
--- trunk/modules/mod_blog_calendar/js/blog_list.js
+++ trunk/modules/mod_blog_calendar/js/blog_list.js
@@ -1,12 +1,14 @@
-    function toggle_visibility(id) {
-       var e = document.getElementById(id);
-       if(e.style.display == 'block'){
-          e.style.display = 'none';
-		  document.getElementById('toggle-'+id).innerHTML="&#9658;";
-		  }
-       else{
-          e.style.display = 'block';
-		  document.getElementById('toggle-'+id).innerHTML="&#9660;";
-		  }
-		  
-    }
+function toggle_visibility(id)
+{
+	var e = $('#' + id);
+	if (e.css('display') == 'block')
+	{
+		e.slideUp();
+		$('#toggle-' +id).html('&#9658;');
+	}
+	else
+	{
+		e.slideDown();
+		$('#toggle-' +id).html('&#9660;');
+	}
+}
