b689e6a0ab6cc69314bdf0bb5ad167c9c2c57657
jar.patryk <jar.patryk@b36344ac-a876-f84d-e60f-d41b6b5e09fb>
2011-03-01 21:26:12 +0000

    dodanie polskiego tlumaczenia

---
+++ components/com_fbchat/languages/polish.js
@@ -0,0 +1,13 @@
+﻿//FILE di traduzione variabili javascript
+
+var chionline = 'Użytkownicy OnLine';
+var gooffline = 'Wyłącz';
+var available = 'Dostępny';
+var statooccupato = 'Zajęty';
+var statoinvisibile = 'Niewidoczny';
+var defaultstatus = '-';
+var mystatus = '<a id="setstatusmessage" href="javascript:void(0)">Ustaw</a> status';
+var addfriend = 'Dodaj znajomegp';
+var chatoptionstitle = 'Opcje';
+var pleaselogin = 'Proszę się zalogować, aby używać chatu'; 
+var spacer = '&nbsp;';  
