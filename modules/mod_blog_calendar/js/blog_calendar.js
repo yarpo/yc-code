c3a235218859f9c156992be18a9b745c30796a4b
jar.patryk <jar.patryk@b36344ac-a876-f84d-e60f-d41b6b5e09fb>
2011-03-01 00:26:45 +0000

    udane przejscie na jquery

--- modules/mod_blog_calendar/js/blog_calendar.js
+++ modules/mod_blog_calendar/js/blog_calendar.js
@@ -1,102 +1,80 @@
 var modid;
-var key;
 
-window.addEvent('domready', function(){
-	var links = document.getElementsByTagName('a');
-
-	for(key in links){
-
-		if(typeof(links[key]) == "object"){
-			if(links[key].id.substring(0,10)=='prevMonth-')
-			{
-				modid= links[key].id.substring(10);
-				links[key].onclick = function()
-				{
-					month--;
-					newDate(month,year,modid);
-				}
-				links[key].href='javascript:void(0)';
-			}
-
-
-			if(links[key].id.substring(0,10)=='nextMonth-')
-			{
-				links[key].onclick = function()
-				{
-					month++;
-					newDate(month,year,modid);
-				}
-				links[key].href='javascript:void(0)';
-			}
-		}
-	}
-});
-
-function newAjax()
+(function() 
 {
-	/* THIS CREATES THE AJAX OBJECT */
-	var xmlhttp=false; 
-	try 
-	{ 
-		// ajax object for non IE navigators
-		xmlhttp=new ActiveXObject("Msxml2.XMLHTTP"); 
-	}
-	catch(e)
+	if (!window.jQuery)
 	{
-		try
-		{
-			// ajax object for IE 
-			xmlhttp=new ActiveXObject("Microsoft.XMLHTTP"); 
-		}
-		catch(E) { xmlhttp=false; }
+		setTimeout(arguments.callee, 1000);
 	}
-	if (!xmlhttp && typeof XMLHttpRequest!="undefined") { xmlhttp=new XMLHttpRequest(); } 
+	else
+	{
+		$(document).ready(setupBlogTable);
+	}
+})();
 
-	return xmlhttp; 
+
+function setupBlogTable()
+{
+	$('a[id^="prevMonth-"]').each(function() 
+	{
+		modid = this.id.substring(10);
+		this.href='javascript:void(0)';
+
+		this.onclick = function()
+		{
+			month--;
+			newDate(month,year,modid);
+			return false;
+		}
+	});
+
+	$('a[id^="nextMonth-"]').each(function() 
+	{
+		modid = this.id.substring(10);
+		this.href='javascript:void(0)';
+
+		this.onclick = function()
+		{
+			month++;
+			newDate(month,year,modid);
+			return false;
+		}
+	});
 }
 
 function newDate(month,year,modid)
 {
-		var myFx = new Fx.Style('tableCalendar-'+modid, 'opacity').start(0);
-		loadHtml  = "<p id='loadingDiv-"+modid+"' style='margin-left: 1cm; margin-top: -2cm; margin-bottom: 2cm;'>";
-		loadHtml += "<img src='"+calendar_baseurl+"modules/mod_blog_calendar/img/loading.gif'>";
-		loadHtml += "Loading...</p>";
-		document.getElementById( 'calendar-'+modid ).innerHTML +=  loadHtml ;
-		var myFx = new Fx.Style('tableCalendar-'+modid, 'opacity').start(1,0.05);
-		
-		if(month<=0){
-			month+=12;
-			year--;
-		}
-		if(month>12){
-			month-=12;
-			year++;
-		}
-
-		var ajax = newAjax();
-		ajax.open("POST", location.href, true);
-		ajax.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-		ajax.send('year='+year+'&month='+month+'&ajaxCalMod=1'+'&ajaxmodid='+modid);
-		ajax.onreadystatechange=function()
-		{
-			if (ajax.readyState==4)
-			{
-			
-				var response = ajax.responseText;
-				var start = response.indexOf('<!--calendar-'+modid+' start-->');
-				var finish = response.indexOf('<!--calendar-'+modid+' end-->');
-
-				justTheCalendar= response.substring(start,finish);
-				var myFx = new Fx.Style('tableCalendar-'+modid, 'opacity').start(0.3,1);
-				document.getElementById( 'calendar-'+modid ).innerHTML=justTheCalendar;
-
-				linkPrev= document.getElementById('prevMonth-'+modid);
-				linkNext= document.getElementById('nextMonth-'+modid);
-
-				linkPrev.onclick= function(){month--; newDate(month,year,modid);}
-				linkNext.onclick= function(){month++; newDate(month,year,modid);}
-
-				//linkNext.href= linkPrev.href= 'javascript:void(0)';
-			}
-		}
+	if (month <= 0)
+	{
+		month += 12;
+		year--;
 	}
+
+	if (month > 12)
+	{
+		month -= 12;
+		year++;
+	}
+
+	var tableDiv = $('#calendar-' + modid )
+		.html('<img src="'+calendar_baseurl+'modules/mod_blog_calendar/img/loading.gif"> proszę poczekać');
+
+	$.ajax({
+		url: location.href,
+		type: 'POST',
+		data: {
+			'year'  : year,
+			'month' : month,
+			'ajaxCalMod' : 1,
+			'ajaxmodid' : modid
+		},
+		success: function(msg)
+		{
+			var start = msg.indexOf('<!--calendar-'+modid+' start-->');
+			var finish = msg.indexOf('<!--calendar-'+modid+' end-->');
+			justTheCalendar = msg.substring(start,finish);
+			tableDiv.hide().html(justTheCalendar).fadeIn();
+			setupBlogTable();
+		}
+	});
+}
